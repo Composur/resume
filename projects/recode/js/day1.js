28534444f4e3bf7efec925905f1ed7e5042e782e
Composur <xutongxw@163.com>
2018-02-07 09:03:43 +0800

    ""

--- projects/recode/js/day1.js
+++ projects/recode/js/day1.js
@@ -39,16 +39,17 @@ var a = typeof  typeof 4+4
 console.log(a)
 
 
-// var a=10
+var a=10
 var b=2
 // console.log(b=a++)
 // console.log("c"+(b=++a))
 
-console.log("?"+(a>10?(b=a):(b=a-2)))
+console.log("sanm"+(a>10?(b=a):(b=a-2)))
 
 
 let c = 1;  
-c+++c;  
+// c+++c; 
+console.log(c+++c) 
 console.log(typeof c+2);
 
 
@@ -69,3 +70,22 @@ console.log(undefined==null)//true
 console.log(undefined===null)//false
 type(undefined)
 type(null)
+
+
+const test={a:1}
+
+function g(test){
+
+    console.log(test)
+}
+g(test)
+
+var obj = {x : 1};
+function foo(o) {
+    o = 100;
+}
+foo(obj);
+console.log(obj.x);
+
+
+console.log({}==={})
