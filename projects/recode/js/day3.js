1b0ee037518a0063e6640e074affa0b07855e97c
Composur <xutongxw@163.com>
2018-02-26 11:05:28 +0800

    “new  fighting”

--- projects/recode/js/day3.js
+++ projects/recode/js/day3.js
@@ -15,3 +15,14 @@ function fn1() {
 var fn = fn1()
 // log(fn)
 fn()
+eval(log('{a:1}')) 
+
+
+// Arry
+var arr=[1,2,3,4,5,6]
+arr.push(7)
+log(arr)
+arr.pop()
+log(arr)
+arr.splice(1,1,2)
+log(arr)
