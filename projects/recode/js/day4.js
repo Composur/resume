a714a3dc55198f1c403d66b5978a776eb250d9f6
Composur <xutongxw@163.com>
2018-03-07 18:06:58 +0800

    "add dom "

--- projects/recode/js/day4.js
+++ projects/recode/js/day4.js
@@ -1,5 +1,5 @@
 
-// const log=require('./common')
+const log=require('./common')
 
 // function factorial(n){
 //     if(n === 1){
@@ -156,3 +156,147 @@ console.log(unshiftt(arr,5))
 //          return ele1+ele2
 //      })
 //      log(reduce)
+
+var str=` 
+我是第一行
+你是第二行
+他是第三行
+
+
+`
+log(str)
+
+var longString = "Long \
+long \
+long \
+string";
+
+
+
+log(longString)
+
+var str1='用反斜杠分行 \
+反斜杠后面一定要紧跟换行符 \
+这里是第三行,反斜杠后面紧跟的是换行符不要有空格，\
+空格会报错 \
+'
+
+log(str1)
+
+// var str = 'C:/\Users/\Document\/node/\index.js'
+console.log(str)
+
+
+var str = 'hello jirengu.com'
+
+log(str[3])
+log(str.charCodeAt(str[4]))
+log(str.slice(str.indexOf('g')))
+log(str.substr(str.indexOf('o'),4))
+log(str.indexOf('l'))
+log(str.charAt(3))
+// log(str.charAt('g'))
+// log(str.substring(str.charAt('g'),str.length))
+
+
+// 随机IP
+
+var ip = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255) + 0)+"."+(Math.floor(Math.random() * 255) + 0)+"."+(Math.floor(Math.random() * 255) + 0);
+console.log(ip)
+
+function getRandIp(){
+    var arrIp=[]
+    for(var i=0;i<4;i++){
+        arrIp[i]=Math.floor(Math.random()*256)
+    }
+    return arrIp.join('.')
+}
+var ip=getRandIp()
+log(ip)
+
+log('生成随机的颜色')
+function getRandColor(){
+    var color='#'
+    var num='0123456789abcdef'
+    for(var i=0;i<6;i++){
+        color+=num[Math.floor(Math.random()*16)]
+    }
+    return color
+}
+var color = getRandColor()
+console.log(color)   // #3e2f1b
+
+// 随机数字
+function getRadom(min,max){
+    return parseInt(Math.random()*(max-min+1)+min)
+}
+var randon=getRadom(55,700)
+log(randon)
+// 随机字符串
+
+function getRandStr(len){
+    var allStr='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ'
+    var str=''
+    for(var i=0;i<len;i++){
+        str+=allStr.charAt(Math.floor(Math.random()*allStr.length))      
+    }
+    return str
+}
+var str=getRandStr(10)
+log(str)
+
+log('Date')
+
+var setTime={
+    atNow:60*1000,
+    threeMin:60*60*1000,
+    eighthour:60*60*24*1000,
+    thirtyDays:60*60*24*30*1000,
+    tewlveMouns:60*60*24*30*12*1000,
+    eightYear:60*60*24*30*12*1000
+}
+
+function friendlyDate(time){
+    var nowTime=Date.now()
+    log(nowTime)
+    log(time)
+    var currentTime=nowTime-time
+        if(currentTime<60000){
+            return '不到一分钟'
+        }else if(currentTime>=setTime.atNow&&currentTime<setTime.threeMin){
+            return '3分钟前'
+        }else if(currentTime>=setTime.threeMin&&currentTime<setTime.eighthour){
+            return '8小时前'
+        }else if(currentTime>=setTime.eighthour&&currentTime<setTime.thirtyDays){
+            return '3天前'
+        }else if(currentTime>=setTime.thirtyDays&&currentTime<setTime.tewlveMouns){
+            return '2个月前'
+        }else  if(currentTime>=setTime.eightYear){
+            return '8年前'
+        }
+    // log(nowTime)
+
+}
+log(friendlyDate('1492350564250'))
+
+
+log('实现一个reduce函数')
+
+
+
+var arr=[1,2,3]
+function reduce_c(arr,current,initial){
+    var  temArr=(initial  ===undefined ? []:[initial]).concat(arr)
+    while(temArr.length>1){
+        temArr.splice(0,2,current(temArr[0],temArr[1]))
+
+    }
+    return temArr[0]
+}
+
+var sum=reduce_c(arr,function(num1,num2){
+    return num1+num2
+})
+log(sum)
+
+
