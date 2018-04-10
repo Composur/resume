44cbe5227f374cb137d56b58aefd540628574915
Composur <xutongxw@163.com>
2018-04-10 11:11:03 +0800

    'add search'

--- projects/douban-movie/v02.js
+++ projects/douban-movie/v02.js
@@ -1,27 +1,35 @@
 var log = console.log.bind(console)
 
  // 代码重构,减少全局变量
+ var Helper = {
+    isToEnd: function($viewport, $content){
+      return $viewport.height() + $viewport.scrollTop() +10 > $content.height()
+    }
+  }
 
 
  var top250 = {
      init: function () {
          this.$container = $('#Top250')
-         this.bind()
-         this.start()
+         this.$content=this.$container.find('.container') 
          this.isLoading = false
          this.startNum = 0
          this.finish = false
-         this.isToBottom()
-
+         this.bind()    
+         this.start()
      },
      bind: function () {
          var _this = this
-         this.$container.find('.container').scroll(function () {
-             _this.start()
-         })
+        this.$container.scroll(function(){
+            log('scroll')
+            if(!_this.finish && Helper.isToEnd(_this.$container,_this.$content)){
+                _this.start()
+            }
+        })
      },
      start: function () {
          var _this = this
+       
          _this.getData(function (data) {
              _this.render(data)
          })
@@ -49,7 +57,7 @@ var log = console.log.bind(console)
              if (_this.startNum >= data.total) {
                  _this.finish = true
              }
-             callback(data)
+             callback&&callback(data)
          }).fail(function (error) {
              console.log(error)
          }).always(function () {
@@ -111,10 +119,222 @@ var log = console.log.bind(console)
 
  }
  var northDirection = {
+    init: function () {
+        this.$container = $('#northDirection')
+        this.bind()
+        this.start()
+        this.isLoading = false
+        this.startNum = 0
+        this.finish = false
+        this.isToBottom()
 
+    },
+    bind: function () {
+        var _this = this
+        this.$container.find('.container').scroll(function () {
+            _this.start()
+        })
+    },
+    start: function () {
+        var _this = this
+        _this.getData(function (data) {
+            _this.render(data)
+        })
+    },
+    getData: function (callback) {
+        // 当执行的过程中又一次的getData请求的话此时的isLoading=true请求就会return掉
+        var _this = this
+        if (_this.isLoading) return
+        _this.isLoading = true
+        // 请求的时候加载loading
+        $('.loading').show()
+        $.ajax({
+            url: 'http://api.douban.com/v2/movie/us_box',
+            type: 'get',
+            // data: {
+            //     start: _this.startNum,
+            //     count: 10
+            // },
+            dataType: 'jsonp'
+        }).done(function (data) {
+            console.log(data)
+            _this.render(data)
+            this.startNum += 10
+            // 判断超出的情况
+            if (_this.startNum >= data.total) {
+                _this.finish = true
+            }
+            callback(data)
+        }).fail(function (error) {
+            console.log(error)
+        }).always(function () {
+            // 请求结束设置为false getData时执行请求
+            _this.isLoading = false
+            // 请求结束去除loading
+            $('.loading').hide()
+        })
+    },
+    render: function (data) {
+        var _this=this
+        data.subjects.forEach(movie => {
+            movie=movie.subject
+            var tpl =
+                ` <div class="item">
+            <a href="#">
+                <div class="cover">
+                    <img src="" alt="">
+                </div>
+                <div class="detail">
+                    <h2></h2>
+                    <div class="extra">
+                        <span class="score"></span><span></span></div>
+                    <div class="extra"><span class='year'></span> / <span class='type'></span></div>
+                    <div class="extra">导演 <span class='director'></span></div>
+                    <div class="extra">主演 /<span class='starring'></span></div>
+                </div>
+            </a>
+        </div>`
+            var $node = $(tpl)
+            $node.find('.cover img').attr('src', movie.images.medium)
+            $node.find('.detail h2').text(movie.title)
+            $node.find('.detail .extra>span').eq(0).text(movie.rating.average + '分')
+            $node.find('.detail .extra>span').eq(1).text(' / ' + movie.collect_count + ' 收藏')
+            $node.find('.year').text(movie.year)
+            $node.find('.type').text(movie.genres.join('、'))
+            $node.find('.director').text(function () {
+                var nameArr = []
+                movie.directors.forEach((name) => {
+
+                    nameArr.push(name.name)
+                })
+                return nameArr.join('、')
+            })
+            $node.find('.starring').text(function () {
+                var StarringArr = []
+                movie.casts.forEach((name) => {
+
+                    StarringArr.push(name.name)
+                })
+                return StarringArr.join('、')
+            })
+            _this.$container.find('.container').append($node)
+        });
+    },
+    isToBottom: function () {
+        return this.$container.find('.container').height() <= this.$container.height() + this.$container.scrollTop() +
+            10
+    }
  }
  var search = {
+    init: function () {
+        this.$container = $('#search-wrap')
+        this.bind()
+        // this.start()
+        this.isLoading = false
+        this.startNum = 0
+        this.finish = false
+        this.isToBottom()
+        this.keyworld=''
 
+    },
+    bind: function () {
+        var _this = this
+        this.$container.find('.container').scroll(function () {
+            _this.start()
+        })
+        this.btn=this.$container.find('.btn').click(function(){
+            _this.keyworld=_this.$container.find('.input-search').val()
+            _this.start()        
+        })
+    },
+    start: function () {
+        var _this = this
+        _this.getData(function (data) {
+            _this.render(data)
+        })
+    },
+    getData: function (callback) {
+        // 当执行的过程中又一次的getData请求的话此时的isLoading=true请求就会return掉
+        var _this = this
+        if (_this.isLoading) return
+        _this.isLoading = true
+        // 请求的时候加载loading
+        $('.loading').show()
+        $.ajax({
+            url: 'http://api.douban.com/v2/movie/search',
+            type: 'get',
+            data: {
+                q: _this.keyworld
+            },
+            dataType: 'jsonp'
+        }).done(function (data) {
+            console.log(data)
+            _this.render(data)
+            this.startNum += 10
+            // 判断超出的情况
+            if (_this.startNum >= data.total) {
+                _this.finish = true
+            }
+            callback(data)
+        }).fail(function (error) {
+            console.log(error)
+        }).always(function () {
+            // 请求结束设置为false getData时执行请求
+            _this.isLoading = false
+            // 请求结束去除loading
+            $('.loading').hide()
+        })
+    },
+    render: function (data) {
+        var _this=this
+        data.subjects.forEach(movie => {
+            // movie=movie.subject
+            var tpl =
+                ` <div class="item">
+            <a href="#">
+                <div class="cover">
+                    <img src="" alt="">
+                </div>
+                <div class="detail">
+                    <h2></h2>
+                    <div class="extra">
+                        <span class="score"></span><span></span></div>
+                    <div class="extra"><span class='year'></span> / <span class='type'></span></div>
+                    <div class="extra">导演 <span class='director'></span></div>
+                    <div class="extra">主演 /<span class='starring'></span></div>
+                </div>
+            </a>
+        </div>`
+            var $node = $(tpl)
+            $node.find('.cover img').attr('src', movie.images.medium)
+            $node.find('.detail h2').text(movie.title)
+            $node.find('.detail .extra>span').eq(0).text(movie.rating.average + '分')
+            $node.find('.detail .extra>span').eq(1).text(' / ' + movie.collect_count + ' 收藏')
+            $node.find('.year').text(movie.year)
+            $node.find('.type').text(movie.genres.join('、'))
+            $node.find('.director').text(function () {
+                var nameArr = []
+                movie.directors.forEach((name) => {
+
+                    nameArr.push(name.name)
+                })
+                return nameArr.join('、')
+            })
+            $node.find('.starring').text(function () {
+                var StarringArr = []
+                movie.casts.forEach((name) => {
+
+                    StarringArr.push(name.name)
+                })
+                return StarringArr.join('、')
+            })
+            _this.$container.find('.container').append($node)
+        });
+    },
+    isToBottom: function () {
+        return this.$container.find('.container').height() <= this.$container.height() + this.$container.scrollTop() +
+            10
+    }
  }
  var app = {
     // 初始化
@@ -123,6 +343,8 @@ var log = console.log.bind(console)
         this.$section = $('section')
         this.bind()
         top250.init()
+        northDirection.init()
+        search.init()
     },
     // 绑定事件,触发的时候执行
     bind: function () {
