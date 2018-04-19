61240a369c80a1a301fbae6305e9d812ed41e95d
Composur <xutongxw@163.com>
2018-04-19 20:35:19 +0800

    "完成底部"

---
+++ projects/music_163/js/index.js
@@ -0,0 +1,7 @@
+$('.tabs').on('click','li',function(e){
+    let $li=$(e.currentTarget)
+    let $index=$li.index()
+    $li.addClass('active').siblings().removeClass('active')
+    let $tabContents=$('.tab-contents')
+    $tabContents.children().eq($index).addClass('active').siblings().removeClass('active')
+})
