81e7a3c84741ab8f158209b14a79ca1e53df720b
Composur <xutongxw@163.com>
2018-05-13 16:48:44 +0800

    '增加本地存储，使刷新有状态 '

--- review/vue/resume02/src/index.js
+++ review/vue/resume02/src/index.js
@@ -13,13 +13,32 @@ var app = new Vue({
     //     //     i+=1
     //     // },1000)
     // }
+
+
+    created: function(){
+        // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
+        window.onbeforeunload = ()=>{
+          let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
+          window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+        }
+        let oldDataString = window.localStorage.getItem('myTodos')
+        let oldData = JSON.parse(oldDataString)
+        this.todoList = oldData || []
+      },
     methods:{
         addTodo:function(){
             this.todoList.push({
                 title:this.newTodo,
-                createdAt:new Date()
+                createdAt:new Date(),
+                // 创建done属性标记完成与否
+                done:false
             })
-            console.log(this.todoList)
+            // 输入完成后input的值清空
+            this.newTodo=''
+        },
+        removeTodo:function(todo){
+            let index=this.todoList.indexOf(todo)
+            this.todoList.splice(index,1)
         }
     }
 })
