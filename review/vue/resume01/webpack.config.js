3889906d50f3908c2e5858743d60b83ff882a397
Composur <xutongxw@163.com>
2018-05-13 14:17:03 +0800

    'hello vue'

--- review/vue/resume01/webpack.config.js
+++ review/vue/resume01/webpack.config.js
@@ -1,9 +1,22 @@
-const path = require('path');
-
+const path=require('path')
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname,'./src/index.js'),
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+    path:path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  // module: {
+  //   loaders:[
+  //     {
+  //       test: /\.js[x]?$/,
+  //       exclude: /node_modules/,
+  //       loader: 'babel-loader?presets[]=es2015&presets[]=react'
+  //     },
+  //   ]
+  // },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
   }
-};
+}
