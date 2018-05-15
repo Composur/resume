be86f03862d0136842336ba0273e7efc64cecf4a
Composur <xutongxw@163.com>
2018-05-15 15:58:58 +0800

    'add resume03  folder'

---
+++ review/vue/resume03/src/index.js
@@ -0,0 +1,44 @@
+import bar from './bar'
+import Vue from  'vue'
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        newTodo:'',
+        todoList:[]
+    },
+    // created : function () {
+    //     let i=0
+    //     // setInterval(()=>{
+    //     //     this.newTodo=i
+    //     //     i+=1
+    //     // },1000)
+    // }
+    created: function(){
+        // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
+        window.onbeforeunload = ()=>{
+          let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
+          window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+        }
+        let oldDataString = window.localStorage.getItem('myTodos')
+        let oldData = JSON.parse(oldDataString)
+        this.todoList = oldData || []
+      },
+    methods:{
+        addTodo:function(){
+            this.todoList.push({
+                title:this.newTodo,
+                createdAt:new Date(),
+                // 创建done属性标记完成与否
+                done:false
+            })
+            // 输入完成后input的值清空
+            this.newTodo=''
+        },
+        removeTodo:function(todo){
+            let index=this.todoList.indexOf(todo)
+            this.todoList.splice(index,1)
+        }
+    }
+})
+bar();
