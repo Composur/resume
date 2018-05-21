9b4266cb3d54300819be3214b2a72f5a23e1a49d
Composur <xutongxw@163.com>
2018-05-21 18:20:22 +0800

    add ele ui

--- review/vue/resume04/src/main.js
+++ review/vue/resume04/src/main.js
@@ -4,8 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import 'normalize.css'
 import './assets/reset.scss'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI);
 
