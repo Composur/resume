806a911730b50f4c3f4d955042ceaac6a4907da9
Composur <xutongxw@163.com>
2018-05-25 10:49:44 +0800

    add dist

--- review/vue/resume04/config/index.js
+++ review/vue/resume04/config/index.js
@@ -42,8 +42,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'resume/review/vue/resume04/dist/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/resume/review/vue/resume04/dist/',
 
     /**
      * Source Maps
