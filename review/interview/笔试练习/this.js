be76f6f353023865d7b6a35279fa00f7c64ac9e5
Composur <xutongxw@163.com>
2018-06-25 11:30:54 +0800

    运算符 ！~ 以及this

--- review/interview/笔试练习/this.js
+++ review/interview/笔试练习/this.js
@@ -1,6 +1,7 @@
+const log=console.log.bind(console)
 var obj = {
     fn: function(a, b){
-        // console.log(this)
+        console.log(this)
     },
     child: {
         fn2: function(){
@@ -17,3 +18,18 @@ var obj = {
  //等价于
  obj.child.fn2.call(obj.chid)    // 所以 this 是 obj.child
 
+/*******************************************************************/
+
+var number = 50;
+var obj = {
+    number: 60,
+    getNum: function () {
+    var number = 70;
+    return this.number;
+}
+}; 
+
+log(obj.getNum());//60
+log(obj.getNum.call(obj))
+log(obj.getNum.call());//50 浏览器环境
+log(obj.getNum.call({number:20}));//20
--- review/interview/笔试练习/运算符/index.js
+++ review/interview/笔试练习/运算符/index.js
@@ -59,3 +59,17 @@ var object3 = {
 // 逗号操作符 对它的每个操作数求值（从左到右），并返回最后一个操作数的值
 var result = (object2, object1, object3) + object1 +-- object1; 
 log(result)
+
+
+
+// !!运算符
+var x = !!"Hello" + (!"world", !!"from here!!");
+//var x=true+(true)=Number(true)+((Number(true)))=1+1
+log(x)//20
+
+
+// ~运算符,按位非(求反码)，转换为二进制后按位取反,计算规则先加一在取反
+const arg=4;
+log(arg)
+log(arg.toString(2))
+log(~arg)===log(-(arg+1))
