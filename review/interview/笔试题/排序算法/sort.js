0895b3bbccf98cd91a17a16c1f77b7eb359a5420
Composur <xutongxw@163.com>
2018-07-03 21:09:45 +0800

    quickSort

--- review/interview/笔试题/排序算法/sort.js
+++ review/interview/笔试题/排序算法/sort.js
@@ -1,22 +1,23 @@
 let arr=[3,5,7,8,9,3,4,5]
 const sort={
     maopao:function(arr){
-        for(let i=0;i<arr.length;i++){
-            for(let j=0;j<arr.length;j++){
-                if(arr[j]>arr[j+1]){
-                    let temp=arr[j]
-                    arr[j]=arr[j+1]
-                    arr[j+1]=temp
+        var len = arr.length;
+        for (var i = 0; i < len - 1; i++) {
+            for (var j = 0; j < len - 1 - i; j++) {
+                if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+                    var temp = arr[j+1];        // 元素交换
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-        return arr
+        return arr;
     },
     quickSort:function(arr){
         if(arr.length<1){
             return arr
         }
-        let arrMiddleNum=arr.splice(Math.floor(arr.length/2),1)
+        let arrMiddleNum=arr.splice(Math.floor(arr.length/2),1)[0]
         let left=[],right=[];
         for(let i=0;i<arr.length;i++){
             if(arr[i]<arrMiddleNum){
@@ -25,7 +26,7 @@ const sort={
                 right.push(arr[i])
             }
         }
-        return this.quickSort(left)
+        return this.quickSort(left).concat([arrMiddleNum],this.quickSort(right))
     }
 }
 // console.log(sort.maopao(arr))
