b99113498e20af09c7db8500219ca0b2f470104f
xutong <xutong1107@gmail.com>
2018-12-28 08:47:32 +0800

    canvas init

--- mik/gulpfile.js
+++ mik/gulpfile.js
@@ -1,26 +1,36 @@
 //导入工具包 require('node_modules里对应模块')
 var gulp = require('gulp'), //本地安装gulp所用到的地方
-    cssmin = require('gulp-minify-css'),//压缩css
-    sourcemaps = require('gulp-sourcemaps'),//方便查找less文件
+    cssmin = require('gulp-minify-css'), //压缩css
+    sourcemaps = require('gulp-sourcemaps'), //方便查找less文件
     notify = require('gulp-notify'),
     plumber = require('gulp-plumber'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    htmlmin = require('gulp-htmlmin'),
     less = require('gulp-less');
 
 gulp.task('less', function () {
     gulp.src(['src/less/*.less']) //多个文件以数组形式传入
         .pipe(sourcemaps.init())
-        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+        .pipe(plumber({
+            errorHandler: notify.onError('Error: <%= error.message %>')
+        }))
         .pipe(less())
         .pipe(cssmin())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('src/style'))
+        .pipe(gulp.dest('src/build/style'))
         .pipe(livereload()); //将会在src/css下生成index.css以及detail.css 
 });
+gulp.task('minify', () => {
+    return gulp.src('./index.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('src/build'));
+});
 
-gulp.task('default', function(){
+gulp.task('default', function () {
     livereload.listen()
-    gulp.watch('src/less/*.less',['less'])
+    gulp.watch('src/less/*.less', ['less'])
 }); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
 
 //gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
