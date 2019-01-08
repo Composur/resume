90d333f0340f8232338bf784baea9a0a576cecd4
xutong <xutong1107@gmail.com>
2019-01-08 14:00:42 +0800

    pen

--- mik/src/js/index.js
+++ mik/src/js/index.js
@@ -16,39 +16,38 @@ const fn = {
     window.onresize = function () {
         fn.getScreenSize()
       },
-      this.draw()
+    this.draw()
     this.getScreenSize()
     this.clearDraw()
   },
   $: function (ele) {
     return document.querySelector(ele)
   },
-  draw: function () {//监听事件
+  draw: function () { //监听事件
     const context = this.$('#canvas')
     context.addEventListener("mousedown", function (e) {
-      fn.using = true//使用中的状态（画和清除等状态）
+      fn.using = true //使用中的状态（画和清除等状态）
       if (fn.clear) { //清的时候就不能再画了
-        fn.ctx.clearRect(e.clientX-5, e.clientY-5, 10, 10); //清除的时候减5是因为，清理坐标是左上角
+        fn.ctx.clearRect(e.clientX - 5, e.clientY - 5, 10, 10); //清除的时候减5是因为，清理坐标是左上角
       } else {
         fn.startX = e.clientX
         fn.startY = e.clientY
       }
     })
     context.addEventListener('mousemove', function (e) {
+      if (!fn.using) {
+        return
+      }
       // 鼠标移动的时候擦除
       if (fn.clear) {
-        if (fn.using) {
-          fn.ctx.clearRect(e.clientX-5, e.clientY-5, 10, 10);
-        }
-      } else {
-        if (fn.using) {
-          fn.endX = e.clientX
-          fn.endY = e.clientY
-          fn.drawLine(fn.startX, fn.startY, fn.endX, fn.endY)
-          // 更新最后一个点的位置为当前的起始位置
-          fn.startX = fn.endX
-          fn.startY = fn.endY
-        }
+        fn.ctx.clearRect(e.clientX - 5, e.clientY - 5, 10, 10);
+      } else { 
+        fn.endX = e.clientX
+        fn.endY = e.clientY
+        fn.drawLine(fn.startX, fn.startY, fn.endX, fn.endY)
+        // 更新最后一个点的位置为当前的起始位置
+        fn.startX = fn.endX
+        fn.startY = fn.endY
       }
     })
     context.addEventListener('mouseup', function (e) {
@@ -68,7 +67,27 @@ const fn = {
     fn.$('#erase').addEventListener('click', function (e) {
       e.stopPropagation()
       fn.clear = !fn.clear
+      // 这样写的坏处是需要控制clear的状态，逻辑不是很清晰
+      if(fn.clear){
+        fn.$('#erase').textContent='画笔'
+      }else{
+        fn.$('#erase').textContent='橡皮擦'
+      }
+      // 遵循一个按钮只做一件事，我们进行绑定两个事件
+      // 1.启用按钮
+      // 2.启用橡皮檫 
     })
+    // 平铺直出，没有明显的bug
+    // erase.onclick=function(){
+    //   fn.clear=true
+    //   this.className='erase'
+    //   pen.className='erase active'
+    // }
+    // pen.onclick=function(){
+    //   fn.clear=false
+    //   this.className='erase'
+    //   erase.className='erase active'
+    // }
   },
   getScreenSize: function () { //获取视口宽高，动态改变画板的宽高，根据屏幕的缩放
     fn.context = fn.$('#canvas')
