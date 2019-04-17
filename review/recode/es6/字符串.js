56a7cd86f289fe4e4a9006b44de578884e71dcad
Composur <xutong1107@gmail.com>
2019-04-17 17:08:25 +0800

    连续赋值

--- review/recode/es6/字符串.js
+++ review/recode/es6/字符串.js
@@ -3,4 +3,9 @@ let str='xutong'
 log(str.padStart(7,'x'))
 log(str.padEnd(7,'x'))
 log(`${str}`)
-
+log('---------')
+var a = {n: 1}; // 第1行
+var b = a; // 第2行
+a.x = a = {n: 2}; // 第3行
+console.log(a.x); // 第4行
+console.log(b); // 第5行
