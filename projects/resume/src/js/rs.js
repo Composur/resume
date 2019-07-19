b789548a9aef9b052554cb620a7767151980f636
Composur <xutong1107@gmail.com>
2019-07-19 17:51:46 +0800

    style:导航

--- projects/resume/src/js/rs.js
+++ projects/resume/src/js/rs.js
@@ -1,7 +1,25 @@
 (function(){
   const fn={
     init(){
-      // alert('--')
+      this.navMenu()
+    },
+    navMenu:function(){
+      var navLis=fn.$('#topNavBar').querySelectorAll('.nav-item')
+      console.log(navLis)
+      navLis.forEach(element => {
+        element.addEventListener('mouseenter', e => {
+          element.children[1].classList.add('active')
+        });
+        element.addEventListener('mouseleave', e => {
+          element.children[1].classList.remove('active')
+        });
+      });
+    },
+    $(ele){
+      return document.querySelector(ele)
+    },
+    $$(ele){
+      return document.querySelectorAll(ele)
     }
   }
   fn.init()
