89437a5551f94d826a036711c463888bf48f06e7
Composur <xutong1107@gmail.com>
2019-07-23 20:28:19 +0800

    feat(resume):update

--- projects/resume/src/js/rs.js
+++ projects/resume/src/js/rs.js
@@ -1,11 +1,10 @@
-(function(){
-  const fn={
-    init(){
+(function () {
+  const fn = {
+    init() {
       this.navMenu()
     },
-    navMenu:function(){
-      var navLis=fn.$('#topNavBar').querySelectorAll('.nav-item')
-      console.log(navLis)
+    navMenu: function () {
+      var navLis = fn.$('#topNavBar').querySelectorAll('.nav-item')
       navLis.forEach(element => {
         element.addEventListener('mouseenter', e => {
           element.children[1].classList.add('active')
@@ -14,13 +13,47 @@
           element.children[1].classList.remove('active')
         });
       });
+
+      const navLitA = fn.$('#topNavBar').querySelectorAll('.nav-item a')
+      navLitA.forEach(a => {
+        a.addEventListener('click', e => {
+          e.preventDefault();
+          const targetId = e.target.getAttribute("href")
+          const top = fn.$(targetId).offsetTop
+          let currentY=window.scrollY
+          // window.scrollTo(0, top - 100)
+          fn.scroll(currentY,top-80)
+        })
+      })
     },
-    $(ele){
+
+    scroll(currentY, targetY) {
+      // 计算需要移动的距离
+
+      let needScrollTop = targetY - currentY
+
+      let _currentY = currentY
+      const timer = setTimeout(() => {
+        // 一次调用滑动帧数，每次调用会不一样
+        const dist = Math.ceil(needScrollTop / 2)
+        _currentY += dist
+        window.scrollTo(_currentY, currentY)
+        // 如果移动幅度小于十个像素，直接移动，否则递归调用，实现动画效果
+        if (needScrollTop > 2 || needScrollTop < -2) {
+          fn.scroll(_currentY, targetY)
+        } else {
+          window.scrollTo(_currentY, targetY)
+          clearTimeout(timer)
+        }
+      }, 16)
+    },
+    $(ele) {
       return document.querySelector(ele)
     },
-    $$(ele){
+    $$(ele) {
       return document.querySelectorAll(ele)
-    }
+    },
+
   }
   fn.init()
 })()
