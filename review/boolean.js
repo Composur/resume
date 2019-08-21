5c9759bd5115bf24d2bf427655317286d231ed97
Composur <xutong1107@gmail.com>
2019-08-21 16:46:28 +0800

    test:reduce

--- review/boolean.js
+++ review/boolean.js
@@ -1,1 +1,13 @@
 if(new Boolean(false)){console.log('--')}
+
+var a = [1,2,3,4,5,6,7,8,9]
+
+
+const reuslt=a.reduce((sum,item)=>{
+  if(item&1){
+   return sum+=item
+  }else{
+    return sum
+  }
+},0)
+console.log(reuslt)
