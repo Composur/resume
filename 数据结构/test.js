40a244c2d7240c078a7dabc384e8e5200dec4c3c
Composur <xutong1107@gmail.com>
2020-02-05 22:17:57 +0800

    二进制

--- 数据结构/test.js
+++ 数据结构/test.js
@@ -1,38 +1,56 @@
 class Stack {
-  constructor(){
-    this.itmes=[]
+  constructor() {
+    this.itmes = []
   }
   // 压栈
-  push(ele){
+  push(ele) {
     this.itmes.push(ele)
   }
   // 出栈，同时返回这个元素
-  pop(ele){
+  pop(ele) {
     return this.itmes.pop(ele)
   }
   // 返回栈顶元素，不修改栈
-  peek(){
-    return this.itmes[this.itmes.length-1]
+  peek() {
+    return this.itmes[this.itmes.length - 1]
   }
   // 是否为空 boolean
-  isEmpty(){
+  isEmpty() {
     return this.itmes.length === 0
   }
   // 获取个数
-  size(){
+  size() {
     return this.itmes.length
   }
-  toString(){
-   return this.itmes.toString()
+  toString() {
+    return this.itmes.toString()
   }
 }
-const s = new Stack()
-s.push(1)
-s.push(2)
-s.push(3)
-s.pop()
-console.log(s.peek())
-console.log(s.isEmpty())
-console.log(s.size())
-console.log(s.toString())
-console.log(s)
+// const s = new Stack()
+// s.push(1)
+// s.push(2)
+// s.push(3)
+// s.pop()
+// console.log(s.peek())
+// console.log(s.isEmpty())
+// console.log(s.size())
+// console.log(s.toString())
+// console.log(s)
+
+// 十进制转二进制
+
+const dec2bin = function (num) {
+  // 定义一个栈
+  const stack = new Stack()
+  // 取模
+  while (num > 0) {
+    stack.push(num % 2)
+    num = Math.floor(num / 2)
+  }
+  let result = ''
+  while (!stack.isEmpty()) {
+    result += stack.pop()
+  }
+  return result
+}
+console.log(dec2bin(10))//1010
