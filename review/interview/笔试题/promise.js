76a877e462c096ef7e9f9100aa198af30b4d2afc
composure <xutong1107@gmail.com>
2020-10-06 20:04:08 +0800

    test:test

--- review/interview/笔试题/promise.js
+++ review/interview/笔试题/promise.js
@@ -1,49 +1,59 @@
 function fn1(callback) {
-    setTimeout(()=>{
-      console.log('fn1')
-      callback()
+    setTimeout(() => {
+        console.log('fn1')
+        callback()
     }, 1000)
-  }
-  
-  function fn2(callback) {
-    setTimeout(()=>{
-      console.log('fn2')
-      callback()
+}
+
+function fn2(callback) {
+    setTimeout(() => {
+        console.log('fn2')
+        callback()
     }, 1000)
-  }
-  
-  function fn3() {
-    setTimeout(()=>{
-      console.log('fn3')
+}
+
+function fn3() {
+    setTimeout(() => {
+        console.log('fn3')
     }, 1000)
-  }
-  
-  
-  
+}
+
+
+
 //   fn1(function(){
 //     fn2(function(){
 //       fn3()
 //     })
 //   })
-var test=new Promise((resolve,reject)=>{
-    
+var test = new Promise((resolve, reject) => {
+
 })
 
-class Animal{
-  constructor(){
-    this.age=18
-  }
-  move(){}
+class Animal {
+    constructor() {
+        this.age = 18
+    }
+    move() {}
 }
-class Person extends Animal{
-  constructor(name){
-    super()
-    this.name=name
-  }
-  tools(){}
+class Person extends Animal {
+    constructor(name) {
+        super()
+        this.name = name
+    }
+    tools() {}
 }
 
-var f= new Person()
+var f = new Person()
 
 
 
+new Promise((resolve, reject) => {
+    resolve('data')
+}).then(
+    value => {
+        console.log(value)
+    },
+    reason => {
+        console.log(reason)
+    },
+)
