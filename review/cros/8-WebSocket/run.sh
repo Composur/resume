1183de48ebee19d08eb649f2122fb683529ebe9c
composure <xutong1107@gmail.com>
2021-01-12 21:15:38 +0800

    test: test

---
+++ review/cros/8-WebSocket/run.sh
@@ -0,0 +1,4 @@
+echo "request server" 
+node serverReq.js
+echo "response server" 
+node serverRes.js
--- review/cros/8-WebSocket/serverReq.js
+++ review/cros/8-WebSocket/serverReq.js
@@ -7,5 +7,5 @@ app.use(express.static(__dirname + '/staticReq'));
 
 
 app.listen(requestPort, function () {
-    console.log('Requester is listening on port '+ requestPort);
+  console.log('Requester is listening on port ' + requestPort);
 });
